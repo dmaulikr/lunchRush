c89bce3aefd22f0298929ea2710f916e9ab04e64
Ryan Dines <ryan.dines@gmail.com>
2017-08-12 23:04:09 -0400

    Added better level layouts.

---
+++ MeatCrush/RecipeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  RecipeViewController.swift
+//  MeatCrush
+//
+//  Created by Ryan Dines on 8/12/17.
+//  Copyright © 2017 Ryan Dines. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RecipeViewController: UIViewController {
+    
+    
+    
+}
